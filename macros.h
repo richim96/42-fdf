4d635e4b8627ee68ad4e4de4d974cef4da29bd94
Riccardo Mei <rmei@c4a12c5.42berlin.de>
2024-07-19 18:30:52 +0200

    update X11 sizes

--- macros.h
+++ macros.h
@@ -6,7 +6,7 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 12:24:05 by rmei              #+#    #+#             */
-/*   Updated: 2024/07/19 12:57:29 by rmei             ###   ########.fr       */
+/*   Updated: 2024/07/19 18:29:23 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 # define MACROS_H
 
 // - X11 screen
-# define WIN_WIDTH 720
-# define WIN_HEIGHT 480
+# define WIN_WIDTH 1280
+# define WIN_HEIGHT 720
 
 // - X11 image
 # define BASE_COLOR "0xFFFFFF"
-# define IMG_SIZE_COEFF 0.66
-# define IMG_W_POS_COEFF 0.25
-# define IMG_H_POS_COEFF 0.35
+# define IMG_WIDTH 960
+# define IMG_HEIGHT 540
+# define IMG_W_POS_COEFF 0.15
+# define IMG_H_POS_COEFF 0.15
 
 // - X11 events
 # define ON_DESTROY 17
